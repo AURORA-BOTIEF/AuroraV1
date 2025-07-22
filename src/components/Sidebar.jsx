392c27bff3e721b7c680a4d187f7040a1931562f
LizetFlores27 <lizet_ff@hotmail.com>
2025-07-22 14:28:59 -0600

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,25 @@
-// src/components/Sidebar.jsx (CÓDIGO FINAL Y COMPLETO)
-//x
 import { Link } from 'react-router-dom';
 import './Sidebar.css';
 import defaultFoto from '../assets/default.jpg';
 
-function Sidebar() {
+function Sidebar({ email, nombre, grupo }) {
   return (
-    // Añadimos la clase 'sidebar' para que los estilos de index.css se apliquen
-    <div id="barraLateral" className="sidebar"> 
+    <div id="barraLateral" className="sidebar">
       <div id="perfilSidebar">
         <img id="fotoPerfilSidebar" src={defaultFoto} alt="Foto perfil" />
-        <div className="nombre" id="nombreSidebar">Usuario</div>
-        <div className="email" id="emailSidebar">usuario@ejemplo.com</div>
+        <div className="nombre" id="nombreSidebar">{nombre || 'Usuario conectado'}</div>
+        <div className="email" id="emailSidebar">{email}</div>
+        <div className="grupo" id="grupoSidebar">🎖️ Rol: {grupo || 'Sin grupo'}</div>
       </div>
 
       <div id="caminito">
-        {/* Módulos (se mantiene sin cambios) */}
+        {/* Módulos */}
         <div className="step">
           <div className="circle">🧠</div>
           <span>Módulos</span>
         </div>
 
-        {/* ACTIVIDADES: Ahora es un Link de navegación */}
+        {/* Actividades (navegable) */}
         <Link to="/actividades" className="nav-link">
           <div className="step" style={{ cursor: 'pointer' }}>
             <div className="circle">📘</div>
@@ -29,7 +27,7 @@ function Sidebar() {
           </div>
         </Link>
 
-        {/* Examen (se mantiene sin cambios) */}
+        {/* Examen */}
         <div className="step">
           <div className="circle">🔬</div>
           <span>Examen</span>
