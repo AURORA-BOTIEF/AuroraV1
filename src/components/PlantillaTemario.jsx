e0cc6093f55e3901aab1fe84cab64bae9d6dbdc8
Mitzi-Montiel <dayannamitzi@gmail.com>
2026-01-08 14:22:28 -0600

    Cambio interfaz plantilla

--- src/components/PlantillaTemario.jsx
+++ src/components/PlantillaTemario.jsx
@@ -5,7 +5,7 @@ import encabezadoImagen from "../assets/encabezado.png";
 import pieDePaginaImagen from "../assets/pie_de_pagina.png";
 import "./EditorDeTemario.css";
 import { Plus, Trash2 } from "lucide-react";
-import "./EditorDeTemario.css";
+import "./EditorDeTemario_Practico.css";
 
 // ===== Utils =====
 const formatDuration = (minutos) => {
